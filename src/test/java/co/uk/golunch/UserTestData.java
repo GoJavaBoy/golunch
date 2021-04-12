65e3927d09f01dd66e6d39ca49ace00bb4efe598
Stasis Yukniavichus <stasys1996@gmail.com>
2021-04-12 17:47:31 +0100

    Add test data for users and restaurants

--- src/test/java/co/uk/golunch/UserTestData.java
+++ src/test/java/co/uk/golunch/UserTestData.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.function.BiConsumer;
 
+import static co.uk.golunch.RestaurantTestData.*;
 import static co.uk.golunch.model.AbstractBaseEntity.START_SEQ;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,7 +21,7 @@ public class UserTestData {
                         @Override
                         public void accept(User a, User e) {
                             assertThat(a).usingRecursiveComparison()
-                                    .ignoringFields("registered", "meals.user").isEqualTo(e);
+                                    .ignoringFields("registered").isEqualTo(e);
                         }
                     },
                     new BiConsumer<>() {
@@ -35,25 +36,33 @@ public class UserTestData {
     public static final int NOT_FOUND = 10;
 
     public static final User user = new User(USER_ID, "User", "user@yandex.ru", "password", Role.USER);
+    public static final User user1 = new User(USER_ID + 2, "User1", "user1@yandex.ru", "password", Role.USER);
+    public static final User user2 = new User(USER_ID + 3, "User2", "user2@yandex.ru", "password", Role.USER);
+    public static final User user3 = new User(USER_ID + 4, "User3", "user3@yandex.ru", "password", Role.USER);
+    public static final User user4 = new User(USER_ID + 5, "User4", "user4@yandex.ru", "password", Role.USER);
+    public static final User user5 = new User(USER_ID + 6, "User5", "user5@yandex.ru", "password", Role.USER);
     public static final User admin = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ADMIN, Role.USER);
 
-//    static {
-//        user.setMeals(meals);
-//        admin.setMeals(List.of(adminMeal2, adminMeal1));
-//    }
+    static {
+        user.setRestaurant(userRestaurantAbsurdBird);
+        user1.setRestaurant(userRestaurantEatActive);
+        user2.setRestaurant(userRestaurantAbsurdBird);
+        user3.setRestaurant(userRestaurantRosaThai);
+        user4.setRestaurant(userRestaurantRosaThai);
+        user5.setRestaurant(userRestaurantRosaThai);
+        admin.setRestaurant(adminRestaurantHoniPoke);
+    }
 
-//    public static User getNew() {
-//        return new User(null, "New", "new@gmail.com", "newPass", 1555, false, new Date(), Collections.singleton(Role.USER));
-//    }
+    public static User getNew() {
+        return new User(null, "New", "new@gmail.com", "newPass", new Date(), Collections.singleton(Role.USER));
+    }
 
-//    public static User getUpdated() {
-//        User updated = new User(user);
-//        updated.setEmail("update@gmail.com");
-//        updated.setName("UpdatedName");
-//        updated.setCaloriesPerDay(330);
-//        updated.setPassword("newPass");
-//        updated.setEnabled(false);
-//        updated.setRoles(Collections.singletonList(Role.ADMIN));
-//        return updated;
-//    }
+    public static User getUpdated() {
+        User updated = new User(user);
+        updated.setEmail("update@gmail.com");
+        updated.setName("UpdatedName");
+        updated.setPassword("newPass");
+        updated.setRoles(Collections.singletonList(Role.ADMIN));
+        return updated;
+    }
 }
