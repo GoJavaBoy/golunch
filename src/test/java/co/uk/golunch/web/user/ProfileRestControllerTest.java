cb73a42f65b9008e16cd3a72be63b15aa0edc595
Stasis Yukniavichus <stasys1996@gmail.com>
2021-04-14 10:45:06 +0100

    Add ProfileRestController

---
+++ src/test/java/co/uk/golunch/web/user/ProfileRestControllerTest.java
@@ -0,0 +1,69 @@
+package co.uk.golunch.web.user;
+
+import co.uk.golunch.model.User;
+import co.uk.golunch.service.UserService;
+import co.uk.golunch.web.AbstractControllerTest;
+import co.uk.golunch.web.json.JsonUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static co.uk.golunch.web.user.ProfileRestController.REST_URL;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static co.uk.golunch.TestUtil.readFromJson;
+import static co.uk.golunch.TestUtil.userHttpBasic;
+import static co.uk.golunch.UserTestData.*;
+
+public class ProfileRestControllerTest extends AbstractControllerTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL)
+                .with(userHttpBasic(user)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(USER_MATCHER.contentJson(user));
+    }
+
+    @Test
+    void getUnAuth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL)
+                .with(userHttpBasic(user)))
+                .andExpect(status().isNoContent());
+        USER_MATCHER.assertMatch(userService.getAll(), admin, user1, user2, user3, user4, user5);
+    }
+
+    @Test
+    void update() throws Exception {
+        User updated = new User(user.getId(), "newName", "newemail@ya.ru", "newPassword", user.getRegistered(), user.getRoles());
+        perform(MockMvcRequestBuilders.put(REST_URL).contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(user))
+                .content(JsonUtil.writeValue(updated)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        USER_MATCHER.assertMatch(userService.get(USER_ID), updated);
+    }
+
+    @Test
+    void getWithRestaurant() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "/with-restaurant")
+                .with(userHttpBasic(user)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(USER_WITH_RESTAURANT_MATCHER.contentJson(user));
+    }
+}
